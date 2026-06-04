de54c3062ae91aec8e5d4100c10ee3f0a2263c54
ThenamXe0n <thenameet0@gmail.com>
2026-06-05 01:53:06 +0530

    first commit

---
+++ apps/api/src/config/env.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || "5000",
+
+  MONGODB_URI: process.env.MONGODB_URI || "",
+
+  JWT_SECRET: process.env.JWT_SECRET || "",
+
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# AI Career & communication coach 
+An AI driven platform to get yourself prepared for interviews or client calls 
+----
